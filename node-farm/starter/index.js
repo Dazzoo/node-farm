0c4fbfe4305105fa0062d558b80dbc1e2676d015
Юра Шушанський <yuraks46@gmail.com>
2023-02-13 21:52:34 +0200

    server

--- node-farm/starter/index.js
+++ node-farm/starter/index.js
@@ -1,4 +1,8 @@
 const fs = require("fs")
+const http = require("http")
+
+//////////////////////////////////
+// FILES
 
 // //Blocking synchrous way
 // const textIn = fs.readFileSync('./txt/input.txt')
@@ -8,14 +12,26 @@ const fs = require("fs")
 // console.log('File written')
  
 // Non-blocking async
-fs.readFile('./txt/start.txt', { encoding : 'utf8' }, (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, { encoding : 'utf8' }, (err, data2) => {
-        console.log(data2)
-        fs.readFile(`./txt/append.txt`, { encoding : 'utf8' }, (err, data3) => {
-            fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, { encoding : 'utf8' }, (err) => {
-                console.log('Your file has been written')
-            })
-        })
-    })
+// fs.readFile('./txt/start.txt', { encoding : 'utf8' }, (err, data1) => {
+//     if (err) { return console.log('ERROR') }
+//     fs.readFile(`./txt/${data1}.txt`, { encoding : 'utf8' }, (err, data2) => {
+//         console.log(data2)
+//         fs.readFile(`./txt/append.txt`, { encoding : 'utf8' }, (err, data3) => {
+//             fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, { encoding : 'utf8' }, (err) => {
+//                 console.log('Your file has been written')
+//             })
+//         })
+//     })
+// })
+// console.log('We will read this')
+
+//////////////////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!')
 })
-console.log('We will read this')
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000')
+})
